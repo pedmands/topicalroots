00079e90d135fc7dcfa0a3d703392a4c17cdb43b
Preston Edmands <pedmands@gmail.com>
2015-12-27 09:22:53 -0500

    Set up Grunt with Sass

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Topical Roots Theme Customizer.
+ *
+ * @package Topical_Roots
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function topicalroots_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'topicalroots_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function topicalroots_customize_preview_js() {
+	wp_enqueue_script( 'topicalroots_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'topicalroots_customize_preview_js' );
